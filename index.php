239fea9cf839781990ac649f1fb046ef90830be2
caseymrobbins <104038119+caseymrobbins@users.noreply.github.com>
2023-12-01 13:35:19 -0600

    Update the comments to clarify

--- index.php
+++ index.php
@@ -5,16 +5,19 @@ function isPrime($number) {
         return false;
     }
 
+    // Handle base cases for 2 and 3
     if ($number == 2 || $number == 3) {
         return true;
     }
 
+    // Check divisibility by 2 and 3
     if ($number % 2 == 0 || $number % 3 == 0) {
         return false;
     }
 
     // Skip divisors of 5
     for ($i = 5; $i * $i <= $number; $i += 6) {
+        // Check for divisibility by 6k ± 1
         if ($number % $i == 0 || $number % ($i + 2) == 0) {
             return false;
         }
@@ -28,27 +31,30 @@ function findNextPrime($number) {
 
     // Ensure the next number is of the form 6k ± 1
     $nextNumber = ($nextNumber % 6 == 0) ? $nextNumber + 1 : $nextNumber;
-    
+
+    // Find the next prime number
     while (true) {
         if (isPrime($nextNumber)) {
             return $nextNumber;
         }
-        $nextNumber += 2;
+        // Skip even numbers
+        $nextNumber += 2; 
     }
 }
-
 
 function findPreviousPrime($number) {
     $previousNumber = $number - 1;
 
     // Ensure the previous number is of the form 6k ± 1
     $previousNumber = ($previousNumber % 6 == 0) ? $previousNumber - 1 : $previousNumber;
-    
+
+    // Find the previous prime number
     while ($previousNumber >= 2) {
         if (isPrime($previousNumber)) {
             return $previousNumber;
         }
-        $previousNumber -= 2;
+        // Skip even numbers
+        $previousNumber -= 2; 
     }
 
     return "No previous prime found";
